bde0e1bd694ad5139fbafaba7cdff4e6df2314d0
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2022-10-07 20:43:11 -0500

    5.0

--- js/main.js
+++ js/main.js
@@ -1,9 +1,41 @@
-$(function() {
-    $("section").each(function() {
+$(function(){
+    $("section").each(function(){
         let title = $(this).find("h2").html();
-        let = idSection = $(this).attr("id")
-        $("#menu-main").append('<li class="nav-item">' +
-        '<a class="nav-link" href="#' + idSeccion + '">' + title +'</a></li>');
-    });
+        let idsection = $(this).attr("id");
+        $("#menu-main").append('<li class="nav-item">' + 
+        ' <a class="nav-link" href="#'+ idsection +'">' + title +' </a>'+  ' </li>');
+    })
+
+
+    $("#menu-item-form").click(function(){
+        fetch("fetch/form.html")
+            .then(response => response.text())
+            .then(data => {
+                console.log(data)
+                $("#main-content").html(data);
+        });   
+
+    })
+
+    $("#menu-item-partners").click(function(){
+        fetch("fetch/partners.html")
+            .then(response => response.text())
+            .then(data => {
+                console.log(data)
+                $("#main-content").html(data);
+        });   
+
+    })
+
+    //ASI INTENTAMOS HACER EL HIDE:(
+
+    // $(".title").click(function() {
+    //     if ($("#channel").show == true) {
+    //         $("#channel").hide
+    //     } else {
+    //         $("#channel").show
+    //     }
+    // })
 
 })
+
